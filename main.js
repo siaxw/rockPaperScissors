26243ea7a773033b0016fc3f1b90a867500c07ee
script <slmsttrv1@gmail.com>
2022-07-03 16:45:14 +0500

    start project

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
-let x = 'Hello World!';
-console.log(x);
+function computerPlay(){
+    let a = [
+        'Rock', 
+        'Paper', 
+        'Scissors'
+    ];
+    let b = a[Math.floor(Math.random() * a.length)];
+    return console.log(b);
+}
+computerPlay();
+
+
+function playRound(playerSelection, computerSelection){
+    // code here
+}
+
+const playerSelection = 'rock';
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerPlay));
