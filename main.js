003e76ff763b6ba6efa723d9b4eff26d82e3eb6c
script <slmsttrv1@gmail.com>
2022-07-03 20:02:23 +0500

    HTML AND CSS done

--- main.js
+++ main.js
@@ -1,19 +1,69 @@
+/*
+// Event listener for btns
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+const output = document.getElementById('output');
+
+rock.addEventListener('click', () => {
+    playerSelection = 'rock';
+})
+
+paper.addEventListener('click', () =>{
+    playerSelection = 'paper';
+})
+
+scissors.addEventListener('click', () =>{
+    playerSelection = 'scissors';
+})
+
+
+// Functions
 function computerPlay(){
-    let a = [
-        'Rock', 
-        'Paper', 
-        'Scissors'
-    ];
+    let a = ['Rock', 'Paper', 'Scissors'];
     let b = a[Math.floor(Math.random() * a.length)];
-    return console.log(b);
+    return b;
 }
 computerPlay();
 
-
 function playRound(playerSelection, computerSelection){
-    // code here
+    let won = 'You Won! Lucky Ass!';
+    let lose = 'You Lose! ugly bad ass!';
+    let tie = 'Try again! Tie!';
+    if(playerSelection == 'Rock' && computerSelection == 'Rock'){
+        output.innerText = `${tie}`;
+        return `${tie}`
+    }else if(playerSelection == 'Rock' && computerSelection == 'Paper'){
+        output.innerText = `${lose}`;
+        return `${lose}`;
+    }else if(playerSelection == 'Rock' && computerSelection == 'Scissors'){
+        output.innerText = `${won}`;
+        return `${won}`;
+    }else if(playerSelection == 'Paper' && computerSelection == 'Rock'){
+        output.innerText = `${won}`;
+        return `${won}`;
+    }else if(playerSelection == 'Paper' && computerSelection == 'Paper'){
+        output.innerText = `${tie}`;
+        return `${tie}`;
+    }else if(playerSelection == 'Paper' && computerSelection == 'Scissors'){
+        output.innerText = `${lose}`;
+        return `${lose}`
+    }else if(playerSelection == 'Scissors' && computerSelection == 'Rock'){
+        output.innerHTML = `${lose}`
+        return `${lose}`;
+    }else if(playerSelection == 'Scissors' && computerSelection == 'Paper'){
+        output.innerText = `${won}`
+        return `${won}`;
+    }else if(playerSelection == 'Scissors' && computerSelection == 'Scissors'){
+        output.innerHTML = '${tie}'
+        return `${tie}`;
+    }
 }
 
-const playerSelection = 'rock';
+let playerSelection;
 const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerPlay));
+playRound(playerSelection, computerSelection);
+*/
+
+
+// Pseudo - code
