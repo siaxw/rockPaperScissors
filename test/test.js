c23e97d64ff28b09cf7b2901976642a813708031
script <slmsttrv1@gmail.com>
2022-07-16 19:47:32 +0500

    v1.0 done

--- test/test.js
+++ test/test.js
@@ -29,30 +29,50 @@ function playRound(playerSelection, computerSelection){
         return `${tie} scissors`;
     // Lose
     }else if(playerSelection === 'rock' && computerSelection == 'paper'){
+        computerScore++;
         return `${lose} paper cover a rock`
     }else if(playerSelection === 'paper' && computerSelection == 'scissors'){
+        computerScore++;
         return `${lose} scissors cutting a paper`
     }else if(playerSelection === 'scissors' && computerSelection == 'rock'){
+        computerScore++;
         return `${lose} rock crushes a scissors`
     // Win
     }else if(playerSelection === 'rock' && computerSelection === 'scissors'){
+        playerScore++;
         return `${win} rock crushes a scissors`
     }else if(playerSelection === 'paper' && computerSelection === 'rock'){
+        playerScore++;
         return `${win} paper cover a rock`
     }else if(playerSelection === 'scissors' && computerSelection == 'paper'){
+        playerScore++;
         return `${win} scissors cutting a paper`
     }
 }
 
-const playerSelection = 'rock';
+
 
 
 const game = () =>{
     for(let i = 0; i < 5; i++){
         const computerSelection = computerPlay();
-        console.log(playRound(playerSelection, computerSelection));
+        const playerSelection = prompt('Please enter your choose: rock, paper or scissors').toLowerCase();
+        if(playerSelection == ''){
+            break;
+        }else if(playerSelection == 'fuck you'){
+            alert('Fuck you too!')
+            break;
+        }
+        playRound(playerSelection, computerSelection);
+    }
 
+    if(playerScore > computerScore){
+        return 'You beat computer great job!'
+    }else if(computerScore > playerScore){
+        return 'You lose to a computer! Shame on you!'
+    }else{
+        return 'You tied! Try Again!'
     }
 }
 
-game();
+console.log(game());
