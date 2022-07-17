9fdf27b5920ba272f0c58ed06a9164d5def2811e
script <slmsttrv1@gmail.com>
2022-07-17 12:42:12 +0500

    js dom variables done

---
+++ main.js
@@ -0,0 +1,15 @@
+const gameStatus = document.getElementById('status');
+
+const userScore = document.getElementById('score1');
+const compScore = document.getElementById('score2');
+
+const userChoose = document.getElementById('choose1');
+const compChoose = document.getElementById('choose2');
+
+const btnRock = document.getElementById('buttonRock');
+const btnPaper = document.getElementById('buttonPaper');
+const btnScissors = document.getElementById('buttonScissors');
+
+
+
+
